bddef3b75b6555fe9228599e9f203df364c99cbf
bezay <bijayacharay@gmail.com>
2017-11-19 20:51:58 +0545

    all

---
+++ Assets/Scripts/EdgeDetect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDetect : MonoBehaviour {
+
+    
+    void Update () {
+        
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 left = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 right = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
+
+        if (pos.x < 0) {
+            pos.x = 0;
+            transform.position = new Vector3(right.x, this.transform.position.y + 3f, this.transform.position.x);
+        }
+        if (pos.x > 1.0) { pos.x = 1;
+            transform.position = new Vector3(left.x, this.transform.position.y + 3f, this.transform.position.x);
+        }
+    }
+}
---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour {
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+  
+    //public Text score;
+
+    Rigidbody2D rigidBody;
+    Quaternion downRoatation;
+    Quaternion forwardRotation;
+    public Transform target;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        downRoatation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+              transform.rotation = forwardRotation;
+              rigidBody.velocity = Vector2.zero;
+              rigidBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+              rigidBody.AddForce(Vector2.right * tapForce, ForceMode2D.Force);
+            /*  Vector3 targetDir = target.position - transform.position;
+              float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+              Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+              transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+
+              transform.Translate(Vector3.up * Time.deltaTime * 2);*/
+        }
+        // transform.rotation = Quaternion.Lerp(transform.rotation, downRoatation, tiltSmooth * Time.deltaTime);
+ 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "ScoreZone")
+        {
+           
+        }
+      
+    }
+}
